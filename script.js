449d746a6b125b06d239e6e918c832e13a36b400
iCode-SugarLand <124052535+iCode-SugarLand@users.noreply.github.com>
2024-05-24 17:27:21 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const audiot = new Audio("sound2.mp3");
+
+window.addEventListener("click", ()=>{
+  audiot.play();
+
+});
+
+const audior = new Audio("sound1.mp3");
+
+window.addEventListener("dblclick", ()=>{
+  audior.play();
+
+});
